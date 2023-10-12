9e992e3f957c113515cff8c6424325ea74c6b02d
Lukács Zsolt <rockdonald2@gmail.com>
2023-10-12 22:23:51 +0300

    ci: added jacoco

--- build/resources/test/mysql/scripts/bibliospring-schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `BS_USER`
-(
-    `username` varchar(64) COLLATE 'utf8_bin' NOT NULL UNIQUE,
-    `password` varchar(64) COLLATE 'utf8_bin' NOT NULL,
-    `id`       int unsigned                   NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `uuid`     varchar(36) COLLATE 'utf8_bin' NOT NULL UNIQUE
-) ENGINE = 'InnoDB'
-  COLLATE 'utf8_bin';
